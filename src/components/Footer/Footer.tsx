cf80b4473f6b33e9acb0f24f2310f8ef578d1dd2
dedahiahmed <dedahiahmed10@gmail.com>
2024-02-23 19:04:17 +0000

    footer button notification

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from "react";
+import SignedOut from "../SignedOut/SignedOut";
 
 export default function Footer() {
   const footerNavs = [
@@ -22,30 +25,35 @@ export default function Footer() {
             matières, les modules et tous les aspects essentiels.
           </p>
           <div className="items-center gap-x-3 space-y-3 sm:flex sm:justify-center sm:space-y-0">
-            <a
-              href="javascript:void(0)"
-              className="block py-2 px-4 text-center text-white font-medium bg-sky-500 duration-150 hover:bg-sky-600 active:bg-sky-700 rounded-lg shadow-lg hover:shadow-none"
-            >
-              créer un compte
-            </a>
-            <a
-              href="javascript:void(0)"
-              className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex"
-            >
-              se connecter
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-5 h-5"
+            <SignedOut>
+              {" "}
+              <a
+                href="signup"
+                className="block py-2 px-4 text-center text-white font-medium bg-sky-500 duration-150 hover:bg-sky-600 active:bg-sky-700 rounded-lg shadow-lg hover:shadow-none"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </a>
+                créer un compte
+              </a>
+            </SignedOut>
+            <SignedOut>
+              <a
+                href="signin"
+                className="flex items-center justify-center gap-x-2 py-2 px-4 text-gray-700 hover:text-gray-500 font-medium duration-150 active:bg-gray-100 border rounded-lg md:inline-flex"
+              >
+                se connecter
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </SignedOut>
           </div>
         </div>
         <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
