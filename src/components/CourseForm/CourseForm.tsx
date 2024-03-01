c7fb5c65c529e0540f1808cc9fe067c6804a897f
dedahiahmed <dedahiahmed10@gmail.com>
2024-03-01 13:33:07 +0000

    make new course responsive

--- src/components/CourseForm/CourseForm.tsx
+++ src/components/CourseForm/CourseForm.tsx
@@ -4,7 +4,10 @@ import DropDown from "../DropDownInput/DropDownInput";
 import semesterList from "@/data/lists/semester-list";
 import courseType from "@/data/lists/type-list";
 import FileInput from "../FileInput/FileInput";
-import { filesTypeList } from "@/data/data-base/file-types/file-types";
+import {
+  filesTypeList,
+  imagesTypeList,
+} from "@/data/data-base/file-types/file-types";
 import { FormProvider, useForm } from "react-hook-form";
 import Navbar from "../Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
@@ -55,44 +58,44 @@ export default function CourseForm() {
   };
 
   return (
-    <div className="bg-gray-100">
-      {" "}
+    <div className="bg-gray-100 ">
       <BrowserRouter>
         <Navbar />
       </BrowserRouter>
       <p className="text-xl text-black font-normal  flex justify-start items-start bg-gray-100  mt-[1rem] ml-[5rem]">
         Ajouter un Cours
-      </p>
-      <div className="w-full px-[20rem] h-full bg-gray-100 pb-[3rem] py-[1rem]  ">
-        <FormProvider {...form}>
+      </p>{" "}
+      <FormProvider {...form}>
+        <div className="lg:pl-[25rem] ">
+          {" "}
           <form
+            className=" p-[1rem] mt-[1.25rem] lg:mt-[2.5rem] bg-white lg:w-[50%] lg:p-[2rem] border lg:rounded-lg lg:shadow-lg"
             onSubmit={handleSubmit(onSubmit)}
-            className=" mt-[1.25rem] lg:mt-[2.5rem] space-y-4 bg-white rounded shadow-lg p-4 px-4 w-full h-full "
             noValidate
           >
             {/*name*/}
+            <div className="flex flex-col  gap-[0.5rem] ">
+              <p className="  font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
+                Titre * :
+              </p>
+              <input
+                placeholder="Le nom de votre entreprise."
+                type="text "
+                className="
+             flex h-[2.8125rem]  items-center gap-[0.625rem] rounded-lg border
+            border-stone-900 py-[0.3125rem] pl-[0.625rem] pr-[2.5rem] font-text  csm:w-[20.625rem] lg:mb-[0.5rem]  lg:h-[3.125rem] w-full lg:gap-[0.625rem] lg:py-[0.9375rem] lg:pl-[0.625rem] lg:pr-[2.5rem]"
+                {...register("title", {
+                  required: "Titre requis.",
+                })}
+              />
+              <p className="  font-text text-[0.875rem] font-[500] leading-[1.375rem]  text-[#C54141] mb-[0.5rem] lg:mb-[1.87rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
+                {errors.title?.message}
+              </p>
+            </div>
 
-            {/* founded*/}
-
-            {/*City */}
-            <p className=" mb-[0.37rem] font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-              Titre* :
-            </p>
-            <input
-              type="text "
-              className="
-             flex  w-full  items-center  rounded-lg border
-            border-stone-900  font-text   mb-[0.5rem]  h-[3.125rem] gap-[0.625rem] py-[0.9375rem] pl-[0.625rem] pr-[2.5rem]"
-              {...register("title", {
-                required: `Vous devez fournir un titre à votre Cours`,
-              })}
-            />
-            <p className=" mb-[1.87rem] font-text text-[0.875rem] font-[500] leading-[1.375rem]  text-[#C54141] lg:mb-[1.87rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-              {errors.title?.message}
-            </p>
             <div className="flex flex-col  gap-[0.5rem] ">
               <p className=" mb-[0.37rem] font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-                semester
+                Semestre* :
               </p>
               <DropDown
                 placeHolder="Sélectionner un semestre."
@@ -100,37 +103,33 @@ export default function CourseForm() {
                 name="semester"
                 list={semesterList}
               />
+              <p className="  font-text text-[0.875rem] font-[500] leading-[1.375rem]  text-[#C54141] lg:mb-[1.87rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
+                {errors.semester?.message}
+              </p>
             </div>
             {/*industry */}
             <div className="flex flex-col  gap-[0.5rem] ">
               <p className=" mb-[0.37rem] font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-                Type du document :
+                type du cours* :
               </p>
               <DropDown
-                placeHolder="Sélectionner un le type du document."
-                requiredMessage=" Vous devez sélectionner le secteur d'activité de votre entreprise."
+                placeHolder="Sélectionner type du cour."
+                requiredMessage=" Vous devez sélectionner le type du cour."
                 name="type"
                 list={courseType}
               />
+              <p className="  font-text text-[0.875rem] font-[500] leading-[1.375rem]  text-[#C54141] lg:mb-[1.87rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
+                {errors.type?.message}
+              </p>
             </div>
-            {/* <div className="flex flex-col  gap-[0.5rem] ">
-            <p className="  font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-              Description sur le support :
-            </p>
-            <textarea
-              placeholder="Décrivez brièvement votre entreprise."
-              className="
-        mb-[0.37rem] flex h-[11.375rem] w-full  items-center gap-[0.625rem] rounded-lg border
-        border-stone-900 py-[0.3125rem] pl-[0.625rem] pr-[1.5rem] font-text  csm:w-[20.625rem] lg:mb-[0.5rem]  lg:h-[11.5625rem]  lg:gap-[0.625rem] lg:py-[0.9375rem] lg:pl-[0.625rem] lg:pr-[2.5rem]"
-            />
-          </div> */}
+
             <div className="flex flex-col  gap-[0.5rem] ">
               {" "}
               <p className=" mb-[0.37rem] font-text text-[0.875rem] font-[500] leading-[1.375rem]  lg:mb-[0.5rem] lg:text-[0.9375rem] lg:leading-[1.25rem]">
-                Fichier(s) :
+                fichier* :
               </p>
               <FileInput
-                acceptedTypes={filesTypeList}
+                acceptedTypes={imagesTypeList}
                 limit={true}
                 valueSetter={setFiles}
                 files={files}
@@ -138,16 +137,21 @@ export default function CourseForm() {
             </div>
 
             <button
-              className="mb-[1.25rem] w-[100%] inline-flex items-center justify-center rounded-lg bg-blue-500 hover:bg-blue-400 px-[2.25rem] py-[0.9375rem]
-"
+              className="mb-[1.25rem] w-[100%] inline-flex
+  items-center justify-center 
+  rounded-lg
+  bg-sky-500 
+  hover:bg-hblue
+px-[2.25rem] py-[0.9375rem]
+  "
             >
-              <p className="font-text text-[0.9375rem] font-[500] leading-[1.375rem] text-white lg:text-[1.125rem] lg:leading-[1.625rem]">
-                Ajouter
+              <p className="font-text text-[0.9375rem]  font-[500] leading-[1.375rem] text-white lg:text-[1.125rem] lg:leading-[1.625rem]">
+                enregistrer
               </p>
             </button>
           </form>
-        </FormProvider>
-      </div>
+        </div>
+      </FormProvider>
     </div>
   );
 }
